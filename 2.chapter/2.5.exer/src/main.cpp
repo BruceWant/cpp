425a355bc9b74fb5530260fc74ca71a15e66f487
Bruce Wang <taolinwant@gmail.com>
2020-05-02 18:09:42 +0800

    solution for exercise 2.5

--- 2.chapter/2.5.exer/src/main.cpp
+++ 2.chapter/2.5.exer/src/main.cpp
@@ -3,7 +3,10 @@
 
 int main()
 {
+    std::cout << typeid('a').name() << '\n';
     std::cout << typeid(L'a').name() << '\n';
+    std::cout << typeid("a").name() << '\n';
+    std::cout << typeid(L"a").name() << '\n';
 
     return 0;
 }
