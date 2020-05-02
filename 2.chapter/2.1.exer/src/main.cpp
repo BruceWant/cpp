116e8011dbf6f9bc8065e81ae069e3ed141b1e70
Bruce Wang <taolinwant@gmail.com>
2020-05-02 17:31:22 +0800

    type's size

---
+++ 2.chapter/2.1.exer/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "short:\t\t" << sizeof(short) << std::endl;
+    std::cout << "int:\t\t" << sizeof(int) << std::endl;
+    std::cout << "long:\t\t" << sizeof(long) << std::endl;
+    std::cout << "long long:\t\t" << sizeof(long long) << std::endl;
+    std::cout << "float:\t\t" << sizeof(float) << std::endl;
+    std::cout << "double:\t\t" << sizeof(double) << std::endl;
+
+    return 0;
+}
